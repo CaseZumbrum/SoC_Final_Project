a90a38be6d871dd3da518c769a3cc6ed70c5559f
casezumbrum <casezumbrum@ufl.edu>
2026-04-14 18:01:57 -0400

    working on cleaning up main

--- Audio_Hardware/Software/main.c
+++ Audio_Hardware/Software/main.c
@@ -25,8 +25,6 @@ static int rIdx3 = 0;
 
 //Begin Main
 
-XSysMon XadcInst;
-
 //Echo Function
 int EchoSoftware(int x)
 {
@@ -126,41 +124,63 @@ u16 TransformSoftware(u16 data, int transformtype) {
   return data;
 }
 
+XStatus init_dma(XAxiCdma *DMA_inst){
+    XAxiCdma_Config *DMA_ConfigPtr  = XAxiCdma_LookupConfig(XPAR_AXI_CDMA_0_BASEADDR);
+    return XAxiCdma_CfgInitialize(DMA_inst, DMA_ConfigPtr,DMA_ConfigPtr->BaseAddress);
+}
+
+XStatus init_adc(XSysMon *ADC_inst){
+    XSysMon_Config* ADC_ConfigPtr = XSysMon_LookupConfig(XPAR_XADC_WIZ_0_BASEADDR);
+    if (ADC_ConfigPtr == NULL) return XST_FAILURE;
+    
+    XSysMon_CfgInitialize(ADC_inst, ADC_ConfigPtr, ADC_ConfigPtr->BaseAddress);
+
+    // Configure the Sequencer
+    // First, set to Safe Mode to allow configuration changes
+    XSysMon_SetSequencerMode(ADC_inst, XSM_SEQ_MODE_SAFE);
+
+    // Enable aux channel  
+    XSysMon_SetSeqChEnables(ADC_inst, XSM_SEQ_CH_AUX01);
+
+    // Set ADC to keep running (not event based)
+    XSysMon_SetSequencerMode(ADC_inst, XSM_SEQ_MODE_CONTINPASS);
+
+    XSysMon_SetSeqInputMode(ADC_inst, XSM_CFR0_DU_MASK);
+    XSysMon_SetAvg(ADC_inst, XSM_AVG_64_SAMPLES);
+    return XST_SUCCESS;
+}
+
 //Main Function:
 
 int main() {
-    XSysMon_Config *ADC_ConfigPtr;
-    XAxiCdma xAxiCdmaInstance;
-    XAxiCdma_Config *DMA_ConfigPtr; 
+
+    XStatus status;
+
+    XSysMon ADC_inst;
+
+    XAxiCdma DMA_inst;
+
     u16 RawData;
-    int mode = 0;
+
+    int mode = 2;
     u16 TransformedData = 0;
+
     u16* gpio = (u16 *) XPAR_XGPIO_0_BASEADDR;
     u16* adc = (u16 *) (XPAR_XADC_WIZ_0_BASEADDR + XSM_TEMP_OFFSET + (17 << 2));
 
     // initialize DMA
-    DMA_ConfigPtr = XAxiCdma_LookupConfig(XPAR_AXI_CDMA_0_BASEADDR);
-    XAxiCdma_CfgInitialize(&xAxiCdmaInstance, DMA_ConfigPtr,
-					DMA_ConfigPtr->BaseAddress);
+    status = init_dma(&DMA_inst);
+    if(status == XST_FAILURE){
+        printf("Failed to init dma");
+        return -1;
+    }
 
     // Initialize the XADC driver
-    ADC_ConfigPtr = XSysMon_LookupConfig(XPAR_XADC_WIZ_0_BASEADDR);
-    if (ADC_ConfigPtr == NULL) return XST_FAILURE;
-    
-    XSysMon_CfgInitialize(&XadcInst, ADC_ConfigPtr, ADC_ConfigPtr->BaseAddress);
-
-    // Configure the Sequencer
-    // First, set to Safe Mode to allow configuration changes
-    XSysMon_SetSequencerMode(&XadcInst, XSM_SEQ_MODE_SAFE);
-
-    // Enable aux channel  
-    XSysMon_SetSeqChEnables(&XadcInst, XSM_SEQ_CH_AUX01);
-
-    // Set ADC to keep running (not event based)
-    XSysMon_SetSequencerMode(&XadcInst, XSM_SEQ_MODE_CONTINPASS);
-
-    XSysMon_SetSeqInputMode(&XadcInst, XSM_CFR0_DU_MASK);
-    XSysMon_SetAvg(&XadcInst, XSM_AVG_64_SAMPLES);
+    status = init_adc(&ADC_inst);
+    if(status == XST_FAILURE){
+        printf("Failed to init adc");
+        return -1;
+    }
 
     printf(
         "Select the type of Software Transform you'd like to perform:\n"
@@ -172,28 +192,32 @@ int main() {
         ); 
     while(1) {
         // Wait for data to finish
-        while ((XSysMon_GetStatus(&XadcInst) & XSM_SR_EOS_MASK) != XSM_SR_EOS_MASK);
+        while ((XSysMon_GetStatus(&ADC_inst) & XSM_SR_EOS_MASK) != XSM_SR_EOS_MASK);
 
         // DMA
 
         // Read channel data
-        RawData = XSysMon_GetAdcData(&XadcInst, XSM_CH_AUX_MIN + 1);
+        RawData = XSysMon_GetAdcData(&ADC_inst, XSM_CH_AUX_MIN + 1);
         
         // Apply Transform from Included Header File
 
         TransformedData = TransformSoftware(RawData, mode);
 
+        gpio[0] = TransformedData;
+
         // Send data to GPIO
-        XStatus Status = XAxiCdma_SimpleTransfer(&xAxiCdmaInstance, (UINTPTR)&TransformedData, (UINTPTR)gpio, sizeof(u16), NULL, NULL);
-        if(Status != XST_SUCCESS) 
-        {
-            print("AXI CDMA 0 Transfer Failed\n\r");
-            // send data manually
-            gpio[0] = *adc;
+        // XStatus Status = XAxiCdma_SimpleTransfer(&DMA_inst, (UINTPTR)adc, (UINTPTR)gpio, sizeof(u16), NULL, NULL);
+        // if(Status != XST_SUCCESS) 
+        // {
+        //     print("AXI CDMA 0 Transfer Failed\n\r");
+        //     // send data manually
+        //     gpio[0] = *adc;
             
-        }
-        while(XAxiCdma_IsBusy(&xAxiCdmaInstance));
+        // }
+        // while(XAxiCdma_IsBusy(&DMA_inst));
     }
 
     return 0;
 }
+
+
