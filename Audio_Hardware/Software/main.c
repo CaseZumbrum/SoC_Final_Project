f435f47dce991b2ebc23f1fafb2fe3662e9ad4db
casezumbrum <casezumbrum@ufl.edu>
2026-04-14 22:29:49 -0400

    clean up main file

--- Audio_Hardware/Software/main.c
+++ Audio_Hardware/Software/main.c
@@ -8,121 +8,8 @@
 #include <xsysmon_hw.h>
 #include "xuartlite_l.h"
 
-//for Echo
-#define DELAY_SIZE 1024
-static int delayBuffer[DELAY_SIZE] = {0};
-static int writeIndex = 0; //use globals for memory.
+#include "effects.h"
 
-//for Reverb
-#define REVERB_SIZE 512
-static int reverb1[REVERB_SIZE] = {0};
-static int reverb2[REVERB_SIZE] = {0};
-static int reverb3[REVERB_SIZE] = {0};
-
-static int rIdx1 = 0;
-static int rIdx2 = 0;
-static int rIdx3 = 0;
-
-//Begin Main
-
-//Echo Function
-int EchoSoftware(int x)
-{
-    x = x >> 1;   // reduce input level
-    int delayed = delayBuffer[writeIndex];
-
-    int wet = delayed >> 1;      // echo level
-    int fb  = delayed >> 3;      // feedback level
-
-    int out = x + wet;
-
-    int newSample = x + fb;
-    
-    //clamp
-    if (newSample > 2047) newSample = 2047;
-    if (newSample < -2048) newSample = -2048;
-
-    delayBuffer[writeIndex] = newSample;
-
-    writeIndex++;
-    if (writeIndex >= DELAY_SIZE)
-        writeIndex = 0;
-
-    // clamp again
-    if (out > 2047) out = 2047;
-    if (out < -2048) out = -2048;
-
-    return out;
-}
-
-
-//Extremely Scuffed Reverb Function (idk how it sounds lol)
-int ReverbSoftware(int x)
-{
-    x = x >> 1; 
-
-    int d1 = reverb1[rIdx1];
-    int d2 = reverb2[rIdx2];
-    int d3 = reverb3[rIdx3];
-
-    int wet = (d1 + d2 + d3) / 3;
-
-    int out = x + (wet >> 1);
-
-    int f1 = x + (d1 >> 2) + (d2 >> 3);
-    int f2 = x + (d2 >> 2) + (d3 >> 3);
-    int f3 = x + (d3 >> 2) + (d1 >> 3);
-
-    reverb1[rIdx1] = f1;
-    reverb2[rIdx2] = f2;
-    reverb3[rIdx3] = f3;
-
-    rIdx1 = (rIdx1 + 1) % REVERB_SIZE;
-    rIdx2 = (rIdx2 + 3) % REVERB_SIZE;
-    rIdx3 = (rIdx3 + 5) % REVERB_SIZE;
-
-    if (out > 2047) out = 2047;
-    if (out < -2048) out = -2048;
-
-    return out;
-}
-
-//Simple Software Effects Function
-
-u16 TransformSoftware(u16 data, int transformtype) {
-  if (transformtype == 0) return data;
-
-  //Normalize + Center
-  int standard = (int)(data >> 4) - 2048;
-
-  //HARD CLIPPING ALGORITHM:
-  if (transformtype == 1) {
-    int threshold = 420; //CAN BE CHANGED!
-
-    if (standard > threshold) standard = threshold;
-    if (standard < -threshold) standard = -threshold;
-
-    return (u16)(standard + 2048);
-  }
-
-  //FUZZ ALGORTHIM:
-  if (transformtype == 2) {
-    int threshold = 500; //CAN BE CHANGED
-    if (standard > threshold) standard = threshold;
-    if (standard < -threshold) standard = -threshold;
-
-    standard = standard * 3;
-    return (u16)(standard + 2048); //TOTAL VALUE OF STANDARD SHOULD NOT EXCEED 4095.
-  }
-  if (transformtype == 3) {
-    return(u16)(EchoSoftware(standard) + 2048);
-  }
-  if (transformtype == 4) {
-    return (u16)(ReverbSoftware(standard) + 2048);
-  }
-  
-  return data;
-}
 
 XStatus init_dma(XAxiCdma *DMA_inst){
     XAxiCdma_Config *DMA_ConfigPtr  = XAxiCdma_LookupConfig(XPAR_AXI_CDMA_0_BASEADDR);
@@ -154,15 +41,17 @@ XStatus init_adc(XSysMon *ADC_inst){
 
 int main() {
 
+    // used for status signals
     XStatus status;
 
+    // adc driver
     XSysMon ADC_inst;
-
+    // dma driver
     XAxiCdma DMA_inst;
 
     u16 RawData;
 
-    int mode = 2;
+    int mode = 0;
     u16 TransformedData = 0;
 
     u16* gpio = (u16 *) XPAR_XGPIO_0_BASEADDR;
@@ -171,18 +60,18 @@ int main() {
     // initialize DMA
     status = init_dma(&DMA_inst);
     if(status == XST_FAILURE){
-        printf("Failed to init dma");
+        print("Failed to init dma");
         return -1;
     }
 
     // Initialize the XADC driver
     status = init_adc(&ADC_inst);
     if(status == XST_FAILURE){
-        printf("Failed to init adc");
+        print("Failed to init adc");
         return -1;
     }
 
-    printf(
+    print(
         "Select the type of Software Transform you'd like to perform:\n"
             "0. No Transform\n"
             "1. Distortion - Hard Clipping\n"
@@ -190,22 +79,19 @@ int main() {
             "3. Delay - Echo\n"
             "4. Reverb\n"
         ); 
+
     while(1) {
         // Wait for data to finish
         while ((XSysMon_GetStatus(&ADC_inst) & XSM_SR_EOS_MASK) != XSM_SR_EOS_MASK);
 
-        // DMA
-
         // Read channel data
         RawData = XSysMon_GetAdcData(&ADC_inst, XSM_CH_AUX_MIN + 1);
-        
         // Apply Transform from Included Header File
-
         TransformedData = TransformSoftware(RawData, mode);
-
+        // send data to PWM module
         gpio[0] = TransformedData;
 
-        // Send data to GPIO
+        // ADC SETUP (NOT AS USEFUL WHEN DOING SOFTWARE EFFECTS)
         // XStatus Status = XAxiCdma_SimpleTransfer(&DMA_inst, (UINTPTR)adc, (UINTPTR)gpio, sizeof(u16), NULL, NULL);
         // if(Status != XST_SUCCESS) 
         // {
