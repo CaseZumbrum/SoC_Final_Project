4f511c7faf44e771cdb217c09cdc8df7d989e07a
CaseZumbrum <casezumbrum@ufl.edu>
2026-04-21 00:42:07 -0400

    working on getting dma on the os

--- Audio_Hardware/Software/mmap-example.py
+++ Audio_Hardware/Software/mmap-example.py
@@ -2,8 +2,19 @@ import os
 import mmap
 
 XADC_BASE_ADDR = 0x44A00000
+ADC_OFFSET = 0x200 + (17 << 2)
+
 PWM_BASE_ADDR = 0x40000000
+
+DMA_BASE_ADDR = 0x44a10000
+DMA_SRC_OFFSET = 0x00000018
+DMA_DEST_OFFSET = 0x00000020
+DMA_START_LENGTH_OFFSET = 0x00000028
+DMA_CR_OFFSET = 0x00
+
 MAP_SIZE = 4096 
+
+LENGTH = 4
 
 def main():
     try:
@@ -23,11 +34,22 @@ def main():
                         flags=mmap.MAP_SHARED, 
                         prot=mmap.PROT_READ | mmap.PROT_WRITE, 
                         offset=PWM_BASE_ADDR)
+        
+        dma = mmap.mmap(fd, 
+                        length=MAP_SIZE, 
+                        flags=mmap.MAP_SHARED, 
+                        prot=mmap.PROT_READ | mmap.PROT_WRITE, 
+                        offset=DMA_BASE_ADDR)
 
-        adc_offset = 0x200 + (17 << 2)
-                
+        # Reset DMA
+        # dma[DMA_CR_OFFSET:DMA_CR_OFFSET+4] = (0x4).to_bytes(4, 'little')
+        
+        dma[DMA_SRC_OFFSET:DMA_SRC_OFFSET+4] = (XADC_BASE_ADDR + ADC_OFFSET).to_bytes(4, 'little')
+        dma[DMA_DEST_OFFSET:DMA_DEST_OFFSET+4] = PWM_BASE_ADDR.to_bytes(4, 'little')
+        print("Starting")
         while True:
-            pwm[0:2] = adc[adc_offset:adc_offset+2]
+            dma[DMA_START_LENGTH_OFFSET:DMA_START_LENGTH_OFFSET+4] = LENGTH.to_bytes(4, 'little')
+            # pwm[0:2] = adc[ADC_OFFSET:ADC_OFFSET+2]
             
     except OSError as e:
         print(e)
