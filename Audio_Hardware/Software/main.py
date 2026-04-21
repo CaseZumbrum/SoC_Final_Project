1ef8e8624a479e8bc820fe8ca7961b4aec27ebcc
Jacob-Mack1 <jacob.mack@ufl.edu>
2026-04-21 15:54:43 -0400

    Updated main with an attempt at Software Transforms

--- Audio_Hardware/Software/main.py
+++ Audio_Hardware/Software/main.py
@@ -1,5 +1,7 @@
 import os
 import mmap
+import sys
+import select
 
 XADC_BASE_ADDR = 0x44A00000
 ADC_OFFSET = 0x200 + (17 << 2)
@@ -22,6 +24,7 @@ def bit_set(n:bytes, k:int):
     # return (n[len(n) - 1 - k//8] & (1 << (k%8))) != 0
 
 def main():
+    mode = 0
     try:
         fd = os.open("/dev/mem", os.O_RDWR | os.O_SYNC)
     except PermissionError:
@@ -51,14 +54,44 @@ def main():
         
         dma[DMA_SRC_OFFSET:DMA_SRC_OFFSET+4] = (XADC_BASE_ADDR + ADC_OFFSET).to_bytes(4, 'little')
         dma[DMA_DEST_OFFSET:DMA_DEST_OFFSET+4] = PWM_BASE_ADDR.to_bytes(4, 'little')
-        print("Starting")
+        print("Software-Side Guitar Pedal\n"
+        "0 - Direct Wire to Output (No transform)\n"
+        "1 - Hard Clipping Transform\n"
+        "2 - Fuzz Transform")
         while True:
+            if select.select([sys.stdin], [], [], 0)[0]:
+                try:
+                    mode = int(sys.stdin.readline().strip())
+                    print(f"Mode changed to {mode}")
+                except:
+                    pass
             # check if bit is set
-            while(not bit_set(dma[DMA_STATUS_OFFSET: DMA_STATUS_OFFSET+4], 1)):
-                status = dma[DMA_STATUS_OFFSET: DMA_STATUS_OFFSET+4]
-                print(status, bit_set(dma[DMA_STATUS_OFFSET: DMA_STATUS_OFFSET+4], 1))
-                pass
-            dma[DMA_START_LENGTH_OFFSET:DMA_START_LENGTH_OFFSET+4] = LENGTH.to_bytes(4, 'little')
+            # do DMA if mode == 0
+            if mode == 0:
+                while(not bit_set(dma[DMA_STATUS_OFFSET: DMA_STATUS_OFFSET+4], 1)):
+                    status = dma[DMA_STATUS_OFFSET: DMA_STATUS_OFFSET+4]
+                    print(status, bit_set(dma[DMA_STATUS_OFFSET: DMA_STATUS_OFFSET+4], 1))
+                    pass
+                dma[DMA_START_LENGTH_OFFSET:DMA_START_LENGTH_OFFSET+4] = LENGTH.to_bytes(4, 'little')
+            #if not doing dma then just do direct software transforms (since we didnt set up a proper buffer)
+            else:
+                raw = int.from_bytes(adc[ADC_OFFSET:ADC_OFFSET+2], 'little')
+                sample = (raw & 0x0FFF)
+                if mode == 1:
+                    sample = max(1000, min(3000, sample))
+                if mode == 2:
+                    midpoint = 2048
+
+                    diff = sample - midpoint
+                    diff = diff * 3
+                    diff = int(diff / (1 + abs(diff)/2048))
+
+                    sample = midpoint + diff
+                sample = sample
+                sample = max(0, min(4095, sample))
+
+                pwm[0:2] = int(sample).to_bytes(2, 'little')
+            
             # pwm[0:2] = adc[ADC_OFFSET:ADC_OFFSET+2]
             
     except OSError as e:
